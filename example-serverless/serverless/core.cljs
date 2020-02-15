720226f76a6ce3a0f886215fff8050f9e40a0c07
Mikkel Gravgaard <mikkel@klokke.dk>
2020-02-15 18:35:13 +0100

    wip

--- example-serverless/serverless/core.cljs
+++ example-serverless/serverless/core.cljs
@@ -5,28 +5,30 @@
 (defonce appstate (atom {(random-uuid) {:title "hello" :done? false}}))
 
 (defn template [body]
-  (str "<html><script>function api(url){fetch(url, {method: 'POST'}).then(res => res.text()).then(body => document.getElementById('app').innerHTML=body)}</script><div id='app'>"
+  (str "<html><script>function api(url, body){fetch(url, {body: body, method: 'POST'}).then(res => res.text()).then(body => document.getElementById('app').innerHTML=body)}</script><div id='app'>Todo:"
     body
-      "</div>New: <input id='new'><button onclick='api(\"/app/new/\" + document.getElementById(\"new\").value)'>Submit</button></html>"))
+      "</div>New: <input id='new'><button onclick='api(\"/app/new\", document.getElementById(\"new\").value)'>Submit</button></html>"))
 
 (defn render [items]
-  (str "<ol>"
-       (->> (for [[id {:keys [title done]}] items]
-              (str "<li><input type='checkbox'" (when done " checked") " onclick='api(\"/app/done/" (str id) "\")'>" title "</li>"))
+  (str "<ul>"
+       (->> (for [[id {:keys [title done]}] (->> items
+                                                 (sort-by (comp :date second)))]
+              (str "<li><label><input type='checkbox'" (when done " checked") " onclick='api(\"/app/done/" (str id) "\")'>" title "</label></li>"))
             (clojure.string/join "\n"))
-       "</ol>"))
+       "</ul>"))
 
 
 (def routes ["/" [["app"
-                   [["" (jadak/resource {:methods {:get {:produces #{"text/html"}
+                   [["" (jadak/resource {:methods {:get {:produces #{"text/html;charset=UTF-8"}
                                                          :response (fn [_] (template
                                                                              (render @appstate)))}}})]
-                    [["/new/" :title] (jadak/resource {:methods {:post {:produces #{"text/html"}
-                                                                        :response (fn [{{{:keys [title]} :route-params} :request}]
-                                                                                    (swap! appstate assoc (random-uuid) {:title title
-                                                                                                                         :date (js/Date.)
-                                                                                                                         :done false})
-                                                                                    (render @appstate))}}})]
+                    ["/new" (jadak/resource {:methods {:post {:consumes #{"text/plain;charset=UTF-8"}
+                                                              :produces #{"text/html"}
+                                                                       :response (fn [{{:keys [body]} :request}]
+                                                                                   (swap! appstate assoc (random-uuid) {:title body
+                                                                                                                        :date (js/Date.)
+                                                                                                                        :done false})
+                                                                                   (render @appstate))}}})]
 
                     [["/done/" :id] (jadak/resource {:methods {:post {:produces #{"text/html"}
                                                                       :response (fn [{{{:keys [id]} :route-params} :request}]
