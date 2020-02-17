486ac5d2e0f9037e1a0638dd1b44ba62ca278357
Mikkel Gravgaard <mikkel@klokke.dk>
2020-02-17 22:50:05 +0100

    Example projects (#2)

---
+++ examples/client-server/src-client/example/core.cljs
@@ -0,0 +1,17 @@
+(ns example.core
+  (:require [reagent.core :as r]))
+
+(defonce !app-state (r/atom nil))
+
+(defn app []
+  (let [{:keys [message]} @!app-state]
+    [:div [:p (or message "hello")]
+     [:button {:on-click #(-> (js/fetch "/api/hello")
+                              (.then (fn [r]
+                                       (.text r)))
+                              (.then js/alert))}
+      "Talk to server"]]))
+
+(defn ^:dev/after-load main []
+  (r/render [app] (js/document.getElementById "app")))
+
